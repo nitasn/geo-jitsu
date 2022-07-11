ee96bab5a33bd15c502fb6e803921f406d26e8a5
nitasn <46115487+nitasn@users.noreply.github.com>
2022-07-11 03:48:15 +0300

    add objects redux-slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import grid from './grid';
+import objects from './objects';
 
 export default configureStore({
   reducer: {
     grid,
+    objects,
   },
 });
