3c536d783d86fbe8125b76e7e6e0eb4723b0e390
nitasn <46115487+nitasn@users.noreply.github.com>
2022-07-15 19:22:55 +0300

    popup styles and some functionality

--- src/menu/Menu.jsx
+++ src/menu/Menu.jsx
@@ -3,27 +3,66 @@ import './menu.css';
 
 const e = React.createElement;
 
-const objects = {};
+const possibleObjects = [
+  'Point',
+  'Circle',
+  'Line Segment',
+  'Angle Bisector',
+  'Perpendicular Bisector',
+  'Math Function',
+];
 
 export default () => {
-  const [isOpen, setIsOpen] = React.useState(false);
+  const [isOpen, setIsOpen] = React.useState(true);
+  const [popupShown, setPopupShown] = React.useState(false);
 
-  const onOpenCloseBtnClick = React.useCallback(() => {
-    setIsOpen((currentValue) => !currentValue);
-  }, [setIsOpen]);
+  const onNewObjectChosen = (nameChosen) => {
+    setPopupShown(false);
+    if (!nameChosen) return;
+    console.log(nameChosen);
+  };
 
   return (
     <div open={isOpen} className="menu">
       <div className="head">
         <h2>Items</h2>
-        <a className="round-btn close-open" onClick={onOpenCloseBtnClick}>
+        <a
+          className="round-btn close-open"
+          onClick={() => setIsOpen((currentValue) => !currentValue)}
+        >
           ⟩
         </a>
       </div>
 
       <div className="hr" />
 
-      <a className="round-btn add-object">+</a>
+      <a className="round-btn add-object" onClick={() => setPopupShown(true)}>
+        +
+      </a>
+
+      <div className="popup-container">
+        <PopUpSelectNewObject popupShown={popupShown} onChosen={onNewObjectChosen} />
+      </div>
     </div>
   );
 };
+
+function PopUpSelectNewObject({ onChosen, popupShown }) {
+  return (
+    <ol className="popup-select-new-object" open={popupShown}>
+      <div className="head">
+        <h2>New Item</h2>
+        <a className="round-btn close-popup" onClick={() => onChosen(null)}>
+          &times;
+        </a>
+      </div>
+      {possibleObjects.map((name, idx) => {
+        return (
+          <li onClick={() => onChosen(name)} key={idx}>
+            {name}
+          </li>
+        );
+      })}
+    </ol>
+  );
+}
