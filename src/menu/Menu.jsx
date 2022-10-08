ad563754733e4f40cadee4406827082cdb9e221f
nitasn <46115487+nitasn@users.noreply.github.com>
2022-10-08 15:56:09 +0300

    split to files

--- src/menu/Menu.jsx
+++ src/menu/Menu.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import * as Ionicons from 'react-ionicons';
-import store from '../redux/store';
-import { removeDrawable, setDrawable, setParams } from '../redux/drawables';
+import { useDispatch } from 'react-redux';
+import { setDrawable } from '../redux/drawables';
 
 import {
   MenuDiv,
@@ -10,28 +8,12 @@ import {
   Head,
   Hr,
   AddObjBtn,
-  ObjectsListDiv,
-  ObjectListItem,
-  ListItemHead,
-  Texts,
-  Type,
-  Desc,
-  EditListItem,
-  EditAreaGrid,
-  EditAreaLabel,
-  EditAreaInput,
-  CancelUpdateDeleteDiv,
-  IconBtn,
-  ListNewObj,
-  NewItemHeader,
-  ClosePopupBtn,
-  PossibleObjectLi,
   SlidingButton,
 } from './style';
 
-import possibleObjects from './possibleObjects';
-import { delPoint, setPoint } from '../redux/points';
-import { roundToDecPlaces } from '../math-utils';
+import { setPoint } from '../redux/points';
+import { MenuNewObj } from './MenuNewObj';
+import { ExistingObjectsList } from './ExistingObjectsList';
 
 export default () => {
   const [isOpen, setIsOpen] = React.useState(false);
@@ -62,227 +44,13 @@ export default () => {
       </Head>
       <Hr />
 
-      <ObjectsList />
+      <ExistingObjectsList />
 
       <AddObjBtn onClick={() => setIsAddOpen(true)} children="+" />
       <MenuNewObj isAddOpen={isAddOpen} onChosen={onNewObjectChosen} />
     </MenuDiv>
   );
 };
-
-function ObjectsList() {
-  const drawables = useSelector((state) => state.drawables);
-  const points = useSelector((state) => state.points);
-
-  return (
-    <ObjectsListDiv>
-      {Object.entries(drawables).map(([label, obj]) => (
-        // 'obj' contains { type, params, color }
-        <ObjectsListItem label={label} {...obj} key={label} />
-      ))}
-      {Object.entries(points).map(([label, coords]) => (
-        <ObjectsListItem
-          label={label}
-          type="Point"
-          params={{ coords }}
-          color="yellow"
-          key={label}
-        />
-      ))}
-    </ObjectsListDiv>
-  );
-}
-
-function ObjectsListItem({ label, type, params, color }) {
-  const [beingEdited, setBeingEdited] = React.useState(false);
-
-  return (
-    /** todo why isn't it called beingEdited?? */
-    <ObjectListItem>
-      {beingEdited ? (
-        <>
-          <ListItemHead open>
-            <Texts>
-              <Type>{type}</Type>
-              <span style={{ color }}>{label}</span>
-            </Texts>
-          </ListItemHead>
-
-          <ItemEditArea
-            {...{ label, type, params }}
-            closeFn={() => setBeingEdited(false)}
-          />
-        </>
-      ) : (
-        <ListItemHead>
-          <Texts>
-            <Type>{type}</Type>
-            <span style={{ color }}>{label}</span>
-            <Desc>{oneLineDescription(type, params)}</Desc>
-          </Texts>
-          <EditListItem onClick={() => setBeingEdited(true)}>✐</EditListItem>
-        </ListItemHead>
-      )}
-    </ObjectListItem>
-  );
-}
-
-function ItemEditArea({ label, type, params: originalParams, closeFn }) {
-  const baseId = React.useId();
-  const dispatch = useDispatch();
-
-  const [tempParams, setTempParams] = React.useState(() =>
-    _mapValuesToStrings(originalParams)
-  );
-
-  const onAnyInputChange = (key, value) => {
-    setTempParams({ ...tempParams, [key]: value });
-  };
-
-  const onUpdateClick = () => {
-    const newParams = _validateAndMapParamsFromStrings(type, tempParams);
-    if (!newParams) {
-      return alert('invalid params.');
-    }
-    dispatch(
-      type === 'Point'
-        ? setPoint([label, newParams.coords])
-        : setParams([label, newParams])
-    );
-    closeFn();
-  };
-
-  const onDeleteClick = () => {
-    dispatch((type == 'Point' ? delPoint : removeDrawable)(label));
-  };
-
-  return (
-    <>
-      <EditAreaGrid>
-        {Object.entries(tempParams).flatMap(([key, value]) => {
-          const id = `${baseId}-${key}`;
-          // we're using id's indead of putting the input inside the label,
-          // so that we can use a grid layout (each row has a label and an input)
-          return [
-            <EditAreaLabel key={`${key}-label`} htmlFor={id}>
-              {key}
-            </EditAreaLabel>,
-            <EditAreaInput
-              id={id}
-              key={`${key}-input`}
-              value={value}
-              onChange={(e) => onAnyInputChange(key, e.target.value)}
-            />,
-          ];
-        })}
-      </EditAreaGrid>
-
-      <EraseCancelUpdate onCancelClick={closeFn} {...{ onUpdateClick, onDeleteClick }} />
-    </>
-  );
-}
-
-function EraseCancelUpdate({ onCancelClick, onUpdateClick, onDeleteClick }) {
-  return (
-    <CancelUpdateDeleteDiv>
-      <IconBtn onClick={onCancelClick} mod="cancel">
-        <Ionicons.CloseOutline color="" />
-      </IconBtn>
-
-      <IconBtn onClick={onUpdateClick} mod="update">
-        <Ionicons.CheckmarkOutline color="" />
-      </IconBtn>
-
-      <IconBtn onClick={onDeleteClick} mod="delete">
-        <Ionicons.TrashOutline color="" />
-      </IconBtn>
-    </CancelUpdateDeleteDiv>
-  );
-}
-
-function _prettyPoint(arrOrLabel) {
-  if (Array.isArray(arrOrLabel)) return `(${_arrToCsv(arrOrLabel)})`;
-  return arrOrLabel; // it's a label
-}
-
-function _arrToCsv(values) {
-  return values.map((x) => roundToDecPlaces(x, 3)).join(', ');
-}
-
-function _mapValuesToStrings(obj) {
-  const entries = Object.entries(obj).map(([k, v]) => {
-    if (Array.isArray(v)) v = _arrToCsv(v); // else - it's a label
-    return [k, v];
-  });
-  return Object.fromEntries(entries);
-}
-
-function MenuNewObj({ onChosen, isAddOpen }) {
-  return (
-    <ListNewObj open={isAddOpen}>
-      <Head>
-        <NewItemHeader>New Item</NewItemHeader>
-        <ClosePopupBtn onClick={() => onChosen(null)} children="&times;" />
-      </Head>
-      {possibleObjects.map((name, idx) => (
-        <PossibleObjectLi onClick={() => onChosen(name)} key={idx}>
-          {name}
-        </PossibleObjectLi>
-      ))}
-    </ListNewObj>
-  );
-}
-
-/**
- * todo
- *
- * 1. on any item params change,
- *    it doesn't trigger canvas re-render rn...
- *    until i drag the grid around...
- *
- * 2. if a drawable uses invalid values (labels of non-existing points)
- *    the drawable won't be drawn, and its box in the menu will be clearly mark;
- *    the wrong label will be red, and on hover a tool tip will explain the problem.
- *
- * 3. points should be listed too.
- */
-
-///////////////////////////////////////////////////////////////////////////////
-///                S P E C I F I C   O B J E C T S   C O D E                ///
-///////////////////////////////////////////////////////////////////////////////
-
-/**
- * upon success, returns a new object (where values like "3, 4." are converted into [3, 4]);
- * upon failure, returns null
- * @returns {object?}
- */
-function _validateAndMapParamsFromStrings(type, params) {
-  const { points } = store.getState();
-
-  // todo the type should play a role...
-
-  const results = { ...params };
-
-  for (const [key, value] of Object.entries(params)) {
-    if (value in points && key != value) continue; // if the value is the label of an existing (different) point
-
-    const asNumArr = value.split(',').map((str) => +str.trim());
-    if (asNumArr.length !== 2 || isNaN(asNumArr[0]) || isNaN(asNumArr[1])) return null;
-
-    results[key] = asNumArr;
-  }
-
-  return results;
-}
-
-function oneLineDescription(type, params) {
-  return Object.values(params).map(_prettyPoint).join(' ');
-
-  switch (type) {
-    case 'LineSegment':
-      return `${_prettyPoint(params.from)} ${_prettyPoint(params.to)}`;
-  }
-}
 
 /* todo:
  * replace the default params behaviour with an empty non-cancellable (but deletable) object.
--- src/menu/possibleObjects.js
+++ src/menu/possibleObjects.js
@@ -1,5 +1,3 @@
-import * as drawablesFns from '../canvas/drawables/ctx-drawables';
-
 /**
  * put a space before each capital letter (except for the first)
  * 
@@ -7,6 +5,7 @@ import * as drawablesFns from '../canvas/drawables/ctx-drawables';
  */
 const withSpaces = (name) => name.replace(/(?<!^)([A-Z])/g, ' $1');
 
+import * as drawablesFns from '../canvas/drawables/ctx-drawables';
 const drawablesNames = Object.keys(drawablesFns).map(withSpaces);
 
 export default ['Point', ...drawablesNames];
